25f8c1ffa2d151c427ec5da1684c18a846d09910
FelipeDuan <felipeduan1@gmail.com>
2025-11-01 22:46:50 -0300

    chore: server setup

--- server.ts
+++
@@ -1,7 +0,0 @@
-import { fastify } from "fastify";
-
-const app = fastify();
-
-app.listen({ port: 3100, host: "0.0.0.0" }).then(() => {
-  console.log("HTTP Server rodando em http://localhost:3100");
-});
